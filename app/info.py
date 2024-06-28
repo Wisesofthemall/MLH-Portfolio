8b8a3de5744b631e66dfccd8facaf7bfafd8995f
Robin Batingan <robin.batingan@gmail.com>
2024-06-28 10:02:10 -0700

    Updated all files with proper information

--- app/info.py
+++ app/info.py
@@ -1,11 +1,11 @@
 
 context = {
-        'title': 'My Personal Portfolio',
-        'about': {'name': 'About Me', 'info': 'From a young age, I was captivated by coding, first discovering my passion in elementary school. Watching a programmer control sprites in an Angry Birds simulation ignited my desire to master this skill. Throughout high school, I enthusiastically enrolled in programming electives, tackled challenges like directing robots, and experimented with web apps, data structures, and HTML/CSS. My curiosity led me to mobile game development, and at 17, I published my first 2D multiplayer shooter. Despite initial rejection from the Play Store due to my age, I successfully launched my app on my 18th birthday. I then expanded my skills to backend technologies, building comprehensive web applications. Now, as a CS major, I continue to pursue my passion with enthusiasm, driven by my achievements and the lessons learned, eager to grow into the software engineer I\'ve always aspired to be. '},
+        'title': 'Robin Batingan Portfolio',
+        'about': {'name': 'About Me', 'info': 'Hello! My name is Robin. I am a 3rd year Computer Science student that loves technology! I hava a strong backgroun in Java and Python with other additional skills. In my freetime I play basketball and rock climb.'},
         'work': 'Work Experience',
         'work_experiences': [
             {
-                'title': 'Site Reliability Engineer',
+                'title': 'Site Reliability Engineer Fellowship',
                 'company': 'MLH Fellowship',
                 'start_date': 'June 2024',
                 'end_date': 'Sep 2024',
@@ -15,37 +15,57 @@ context = {
                 ]
             },
             {
-                'title': 'Tech Fellow',
-                'company': 'CodePath',
-                'start_date': 'May 2024',
-                'end_date': 'Aug 2024',
+                'title': 'Automation Intern',
+                'company': 'Lockheed Martin',
+                'start_date': 'June 2023',
+                'end_date': 'Sep 2023',
                 'responsibilities': [
-                    'Answered 100+ technical questions on Slack and during breakout sessions',
-                    'Taught in 30+ breakout room sessions and encouraging the use of the UMPIRE method, resulting in a 70% improvement in problem-solving efficiency'
+                    'Developed and implemented Ansible automation for report generation, improving team efficiency by 90%',
+                    'Collaborated with a database admin mentor to solve Jira tickets e.g. creating and backing up SQL databases'
+                ]
+            },
+            {
+                'title': 'SMART Intern',
+                'company': 'California State University - Los Angeles',
+                'start_date': 'June 2022',
+                'end_date': 'Aug 2022',
+                'responsibilities': [
+                    'Developed a Python-based image recognition program using Intel DevCloud, effectively identifying and extracting images that met predefined criteria, with a focus on bird recognition',
+                    'Conducted EEG data collection and analysis for various tasks and conditions'
+                ]
+            },
+                        {
+                'title': 'Fire Control Technician',
+                'company': 'United STates Navy',
+                'start_date': 'May 2015',
+                'end_date': 'May 2021',
+                'responsibilities': [
+                    'Orchestrated the deployment and upkeep of the Tomahawk computer system, encompassing software installations, user management, and proactive system update administration to sustain optimal equipment readiness',
+                    'Led 5-person team to conduct simulated Tomahawk missile firing sequences in joint multi-ship exercises'
                 ]
             }
         ],
         'hobby': 'Hobbies',
         'hobbies': [
             {
-                'name': 'Binge Watching',
-                'image': ['./static/img/Power Book II.jpeg'],
-                'description': 'I enjoy watching a variety of topics on Netflix, from science fiction to action. My favorite book is "Power Book II".'
+                'name': 'Basketball',
+                'image': ['./static/img/jimmyButler.jpg','./static/img/vicWemb.jpg'],
+                'description': 'I enjoy both playing and watching basketball.'
             },
             {
-                'name': 'Gaming',
-                'image': ['./static/img/Brawlhalla.jpeg', './static/img/Madden.jpeg', './static/img/2k24.jpeg'],
-                'description': 'I love playing multiplayer games with my friends. Some of the games I love to play include Brawlhalla, Madden, and 2k24.'
+                'name': 'Photography',
+                'image': ['./static/img/pic1.png', './static/img/pic3.png', './static/img/pic2.png'],
+                'description': 'I like to take pictuers of the things around me!'
             }
         ],
         'education': 'Education',
         'education_entries': [
             {
-                'degree': 'Computer Science Major',
-                'institution': 'University of Tampa, Tampa FL',
-                'graduation_date': 'Expected Graduation: 2026',
-                'details': 'Relevant Coursework: Data Structures and Algorithms, Discrete Math, Computer Architecture, Operating Systems, Software Engineering'
+                'degree': 'BS in Computer Science',
+                'institution': 'North Seattle College',
+                'graduation_date': 'Expected Graduation: June 2025',
+                'details': 'Relevant Coursework: Data Structures and Algorithms, Operating Systems, Databases, Programming Languages'
             }
         ],
-        'places': 'Places',
+        'places': 'Travel Map',
     }
