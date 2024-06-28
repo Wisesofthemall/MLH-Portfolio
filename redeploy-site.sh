bdcd031d9e0141a8d70197ba6cfb37301888a771
Wisesofthemall <foxxydieujuste@gmail.com>
2024-06-27 23:56:43 -0400

    debug: Added Killing port logic 🐛

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-
-
 # Commands to execute after attaching to tmux session
 cd ~/MLH-Portfolio && echo "Changed directory to ~/MLH-Portfolio"
 
@@ -12,6 +10,11 @@ chmod +x python3-virtualenv/bin/activate && echo "Adding execute permissions to 
 source python3-virtualenv/bin/activate && echo "Activated Python virtual environment"
 
 pip install -r requirements.txt && echo "Installed latest dependencies"
+
+if lsof -Pi :5000 -sTCP:LISTEN -t >/dev/null ; then
+    echo "Flask process found on port 5000. Killing it..."
+    kill -9 $(lsof -ti :5000)
+fi
 
 flask run --host=0.0.0.0 && echo "Started Flask server"
 
