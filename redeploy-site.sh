663984015bef8c327024f8b3c2c8b339f151a929
Wisesofthemall <foxxydieujuste@gmail.com>
2024-06-27 22:50:56 -0400

    debug: Added Logic to create 1 extra tmux session and delete the old one

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,20 +1,47 @@
-#!/bin/sh
+#!/bin/bash
 
-tmux kill-server
+# Get the current tmux session name
+CURRENT_SESSION=$(tmux display-message -p '#S')
 
-cd ~/MLH-Portfolio
-echo "Changed directory to ~/MLH-Portfolio"
+# Set OLD_SESSION to the current session name
+OLD_SESSION="$CURRENT_SESSION"
 
-git fetch && git reset origin/main --hard
-echo "Pulled latest changes from GitHub"
+# Increment NEW_SESSION based on the current session name
+if [[ "$CURRENT_SESSION" =~ ([0-9]+)$ ]]; then
+    VERSION=$((${BASH_REMATCH[1]} + 1))
+    NEW_SESSION="${CURRENT_SESSION%.[0-9]*}.$VERSION"
+else
+    NEW_SESSION="$CURRENT_SESSION.1"
+fi
 
-source python3-virtualenv/bin/activate
-echo "Activated Python virtual environment"
+# Create a new tmux session
+tmux new-session -d -s "$NEW_SESSION"
 
-pip install -r requirements.txt
-echo "Installed latest dependencies"
+# Change directory and update codebase within the new session
+tmux send-keys -t "$NEW_SESSION" 'cd ~/MLH-Portfolio' Enter
+tmux send-keys -t "$NEW_SESSION" 'echo "Changed directory to ~/MLH-Portfolio"' Enter
 
-tmux new-session -d -s flask_server "cd ~/MLH-Portfolio && source python3-virtualenv/bin/activate && flask run --host=0.0.0.0"
-echo "Started Flask server"
+tmux send-keys -t "$NEW_SESSION" 'git fetch && git reset origin/main --hard' Enter
+tmux send-keys -t "$NEW_SESSION" 'echo "Pulled latest changes from GitHub"' Enter
+
+# Activate virtual environment and install dependencies
+tmux send-keys -t "$NEW_SESSION" 'source python3-virtualenv/bin/activate' Enter
+tmux send-keys -t "$NEW_SESSION" 'echo "Activated Python virtual environment"' Enter
+
+tmux send-keys -t "$NEW_SESSION" 'pip install -r requirements.txt' Enter
+tmux send-keys -t "$NEW_SESSION" 'echo "Installed latest dependencies"' Enter
+
+# Start Flask server within the new session
+tmux send-keys -t "$NEW_SESSION" 'flask run --host=0.0.0.0 &' Enter
+tmux send-keys -t "$NEW_SESSION" 'echo "Started Flask server"' Enter
+
+# Wait briefly for Flask to start (adjust sleep time as needed)
+sleep 5
+
+# Check if the old session exists and delete it
+if tmux has-session -t "$OLD_SESSION" 2>/dev/null; then
+    tmux kill-session -t "$OLD_SESSION"
+    echo "Deleted old tmux session: $OLD_SESSION"
+fi
 
 echo "Site redeployed successfully!"
