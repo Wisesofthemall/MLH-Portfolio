7e8d08eda664b04437b019774242e94b85d5861f
Wisesofthemall <foxxydieujuste@gmail.com>
2024-06-29 12:57:02 -0400

    chore: Decrease CI/CD Time

--- run_test.sh
+++ run_test.sh
@@ -28,7 +28,7 @@ kill_flask_process
 start_flask_server() {
     flask run --host=0.0.0.0 --port=5001 &
     flask_pid=$!
-    sleep 3 # Wait for Flask to start
+    sleep 2 # Wait for Flask to start
 }
 
 # Retry logic to start Flask server
@@ -38,7 +38,7 @@ flask_started=false
 
 while [ $retry_count -lt $max_retries ]; do
     start_flask_server
-    sleep 3 # Wait for Flask to start
+    sleep 2 # Wait for Flask to start
     if ps -p $flask_pid > /dev/null; then
         echo "Started Testing Flask server"
         flask_started=true
