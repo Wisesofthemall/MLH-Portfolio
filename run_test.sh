41a506d759a801f91ebb222746e7de008db8021f
Wisesofthemall <foxxydieujuste@gmail.com>
2024-07-11 12:53:43 -0400

    debug: Debugging CI Time 🪰

--- run_test.sh
+++ run_test.sh
@@ -23,6 +23,7 @@ else
 fi
 
 # Run pytest and capture the exit status
+echo "Running unit tests"
 pytest tests/unit
 TEST_STATUS=$?
 if [ $TEST_STATUS -eq 0 ]; then
@@ -32,17 +33,22 @@ else
     exit 1
 fi
 
+echo "Running Database tests"
 chmod +x ./test_db.sh
 ./test_db.sh
 DB_TEST_STATUS=$?
+end_time=$(date +%s)
+elapsed=$((end_time - start_time))
 if [ $DB_TEST_STATUS -eq 0 ]; then
     echo -e "\033[32mDatabase CI passed\033[0m"
+    echo -e "\033[38;5;208mCI Pipeline Execution time: $elapsed seconds\033[0m"
+    exit 0
 else
     echo -e "\033[31mDatabase CI failed\033[0m"
+    echo -e "\033[38;5;208mCI Pipeline Execution time: $elapsed seconds\033[0m"
     exit 1
 fi
 
-end_time=$(date +%s)
-elapsed=$((end_time - start_time))
-echo -e "\033[38;5;208mCI Pipeline Execution time: $elapsed seconds\033[0m" # Orange color
 exit 0
+
+
