29451a97641a77cf04e0b0c3b329676cda630356
Robin Batingan <robin.batingan@gmail.com>
2024-07-19 16:57:14 -0700

    add timeline button features

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, redirect, render_template, request, url_for
 from dotenv import load_dotenv
-from app.info import context
 from peewee import *
 import datetime
 from playhouse.shortcuts import model_to_dict
-
 
 load_dotenv()
 app = Flask(__name__)
@@ -28,65 +26,45 @@ class TimelinePost(Model):
     class Meta:
         database = mydb
 
-
 mydb.connect()
 mydb.create_tables([TimelinePost])
 
-"""
-    View function for each route. (e.g. /, /about, /work, /hobby, /education, /place)
-
-    Renders the html template with the 'context' dictionary and the 'URL' environment variable.
-
-    Returns:
-        Rendered HTML template.
-"""
-
-# Define routes and corresponding view functions
 @app.route('/')
 def index():
-    return render_template('index.html', **context, url=os.getenv("URL"))
+    return render_template('index.html',  url=os.getenv("URL"))
 
 @app.route('/about')
 def about():
-    return render_template('about.html', **context, url=os.getenv("URL"))
+    return render_template('about.html',  url=os.getenv("URL"))
 
 @app.route('/work')
 def work():
-    return render_template('work.html', **context, url=os.getenv("URL"))
+    return render_template('work.html',  url=os.getenv("URL"))
 
 @app.route('/hobby')
 def hobby():
-    return render_template('hobby.html', **context, url=os.getenv("URL"))
+    return render_template('hobby.html',  url=os.getenv("URL"))
 
 @app.route('/education')
 def education():
-    return render_template('education.html', **context, url=os.getenv("URL"))
+    return render_template('education.html',  url=os.getenv("URL"))
 
 @app.route('/place')
 def place():
-    return render_template('place.html', **context, url=os.getenv("URL"))
+    return render_template('place.html', url=os.getenv("URL"))
 
 def navbar():
-    return render_template('navbar.html', **context, url=os.getenv("URL"))
+    return render_template('navbar.html',  url=os.getenv("URL"))
 
-@app.route('/timeline_post', methods = ['POST'])
+@app.route('/timeline_post', methods=['POST'])
 def post_time_line_post():
     name = request.form['name']
     email = request.form['email']
     content = request.form['content']
     timeline_post = TimelinePost.create(name=name, email=email, content=content)
-
-    return model_to_dict(timeline_post)
-
-@app.route('/timeline_post', methods=['GET'])
-def get_time_line_post():
-    return{
-        'timeline_posts': [
-            model_to_dict(p)
-            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-        ]
-    }
+    return redirect(url_for('timeline'))
 
 @app.route('/timeline')
 def timeline():
-    return render_template('timeline.html', title="Timeline")
+    posts = TimelinePost.select().order_by(TimelinePost.created_at.desc())
+    return render_template('timeline.html', timeline_posts=posts, title="Timeline")
