5393082b00dc0fc0ae8b64ce9cc43df903f333ca
Wisesofthemall <foxxydieujuste@gmail.com>
2024-07-29 06:41:24 -0400

    test: Testing CI/CD on Docker container

--- app/info.py
+++ app/info.py
@@ -3,7 +3,7 @@ This module defines the context dictionary used to render HTML templates.
 """
 
 context = {
-    'name': 'Lovinson Dieujuste  ',
+    'name': 'Lovinson Dieujuste 🚀 ',
     'role': 'Site Reliability Engineer',
     'title': 'My Personal Portfolio',
     'about': (
