9f355c07f6126f54dd97eefcdc9754dc90111e8b
aditya-chandraker <chandraker@gmail.com>
2024-08-02 14:14:10 +0000

    progress on testing

--- app/__init__.py
+++ app/__init__.py
@@ -26,13 +26,20 @@ from app.info import context
 
 load_dotenv()
 
-mydb = MySQLDatabase(
-    os.getenv('MYSQL_DATABASE'),
-    user=os.getenv('MYSQL_USER'),
-    password=os.getenv('MYSQL_PASSWORD'),
-    host=os.getenv('MYSQL_HOST'),
-    port=3306
-)
+from peewee import *
+import os
+
+if os.getenv('TESTING') == 'true':
+    print("Running in test mode")
+    mydb = SqliteDatabase(':memory:')
+else:
+    mydb = MySQLDatabase(
+        os.getenv('MYSQL_DATABASE'),
+        user=os.getenv('MYSQL_USER'),
+        password=os.getenv('MYSQL_PASSWORD'),
+        host=os.getenv('MYSQL_HOST'),
+        port=3306
+    )
 
 
 class TimelinePost(Model):
---
+++ tests/test_app.py
@@ -0,0 +1,64 @@
+import unittest
+import os
+os.environ['TESTING'] = 'true'
+
+from app import app
+
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+
+    def test_home(self):
+        response = self.client.get("/")
+        assert response.status_code == 200
+        html = response.get_data(as_text=True)
+        # TODO: Add more tests relating to the home page
+        
+
+    def test_timeline(self):
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        assert response.is_json
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 0
+        # TODO: Add more tests relating to the timeline page
+
+    def test_malformed_timeline_post(self):
+        # POST request missing name
+        response = self.client.post("/api/timeline_post", data={"email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid name" in html
+
+        # POST request with empty content
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": ""})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid content" in html
+
+        # POST request with malformed email
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "not-an-email", "content": "Hello world, I'm John!"})
+        assert response.status_code == 400
+        html = response.get_data(as_text=True)
+        assert "Invalid email" in html
+
+    def test_create_timeline_post(self):
+        # Create a new timeline post
+        response = self.client.post("/api/timeline_post", data={"name": "John Doe", "email": "john@example.com", "content": "Hello world, I'm John!"})
+        assert response.status_code == 200
+        json = response.get_json()
+        assert "id" in json
+
+        # Get the timeline again
+        response = self.client.get("/api/timeline_post")
+        assert response.status_code == 200
+        json = response.get_json()
+        assert "timeline_posts" in json
+        assert len(json["timeline_posts"]) == 1
+        assert json["timeline_posts"][0]["name"] == "John Doe"
+        assert json["timeline_posts"][0]["email"] == "john@example.com"
+        assert json["timeline_posts"][0]["content"] == "Hello world, I'm John!"
+
+if __name__ == '__main__':
+    unittest.main()
